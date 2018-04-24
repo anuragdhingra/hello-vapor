0ee90b50e84062618fd20c3bf1cde81cd7b07aed
risingone <anuragdhingra101@gmail.com>
2018-04-24 08:12:47 +0530

    Adding basic GET and POST routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,10 @@
 import Routing
 import Vapor
 
+struct InfoData: Content {
+    let name: String
+}
+
 /// Register your application's routes here.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
@@ -9,10 +13,30 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    router.get("hello","dhingra") { req in
+        return "Hello, Dhingra!"
+    }
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello \(name)"
+    }
+    
+    
+    router.post(InfoData.self, at: "info/string") { req, data -> String in
+        return "Hello \(data.name)!"
+    }
+    
+    
+    router.post(InfoData.self, at: "info/json") { req, data -> InfoResponse in
+        return InfoResponse(request: data)
+    }
 }
+    
+    struct InfoResponse : Content {
+        let request: InfoData
+    }
+
+
+
